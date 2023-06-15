8bd77a71db9fc8c9a34a45ce20113709302e1c9f
venky <venkateshvanjarapu1622@gmail.com>
2023-06-15 21:29:02 +0530

    video 8 completed added some thing to the site

--- src/components/textform/index.js
+++ src/components/textform/index.js
@@ -1,9 +1,37 @@
 import React , {useState} from 'react'
 
 export default function TextForm() {
-    const handleClick =()=>{
-        console.log("uppercase clicked")
+    // const words=(text,x)=>{
+    //     let n=text.length
+    //     for(var i=n-1;i>=0;i--)
+    //     {
+    //         // eslint-disable-next-line eqeqeq
+    //         if(text[i]==' ')
+    //         {
+    //             i-=1;
+    //             if(i==n-1)
+    //             {
+    //                 x--;
+    //             }
+    //             while(text[i]=='' && i>=0)
+    //             {
+    //                 x--;
+    //                 i--;
+    //             }
+    //             i+=1;
+    //         }
+    //     }
+    //     return x;
+    // }
+   
+    const handleUpClick =()=>{
+        // console.log("uppercase clicked")
         let newText = text.toUpperCase();
+        setText(newText); 
+    }
+    const handleLoClick =()=>{
+        // console.log("lowercase clicked")
+        let newText = text.toLowerCase();
         setText(newText); 
     }
     const handelOnChange =(event)=>{
@@ -11,13 +39,22 @@ export default function TextForm() {
         setText(event.target.value)
     }
     const [text , setText]  = useState("kjekefjk");
+    //  let numberOfWords=text.trim().length
     return (
         <>
             <div className="mb-3">
             <h1>Enter you Text for Upper case</h1>
                 <div className="container">
-                    <textarea className="form-control" value={text} onChange={handelOnChange} id="myBox" rows="8"></textarea>
-                <button className="btn btn-primary" onClick={handleClick}>Click here for UPPERCASE</button>
+                    <textarea className="form-control " value={text} onChange={handelOnChange} id="myBox" rows="8"></textarea>
+                <button className="btn btn-primary mt-3 mx-3" onClick={handleUpClick}>Click here for UPPERCASE</button>
+                <button className="btn btn-primary mt-3 " onClick={handleLoClick}>Click here for LOWERCASE</button>
+                 </div>
+                 <div className="container my-2">
+                    <h2>Your Text Summary</h2>
+                    <p> your text contains {text.trim().split(/\s+/).length  } words {text.length} characters</p>
+                    <p> {0.008 *text.split(' ').length} minutes required to read</p>
+                    <h3>PREVIEW</h3>
+                    <p>{text}</p>
                  </div>
             </div>
             </>
